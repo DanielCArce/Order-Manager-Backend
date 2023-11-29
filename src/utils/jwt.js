772af263b3f5ef79a187ac8de10c2d9d1f4db9ed
DanielCArce <daniel.camposarce@gmail.com>
2023-11-29 16:32:23 -0600

    fix: add error to utils
    adding error handler to the jsonwebtoken

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import ApiError from './Error.js';
 
 export async function create_jwt(payload) {
     
@@ -6,17 +7,20 @@ export async function create_jwt(payload) {
         let jwt_token = await jwt.sign(payload, process.env.TOKEN_SECRET, { expiresIn: '1h' })
         return jwt_token    
     } catch (error) {
-        throw new Error('Error generating the token')
+        throw new ApiError('Token is not generated','Token',error.message)
     }
     
 }
 
 export async function validate_jwt(token) {
+    if (token === undefined || token === null) {
+        throw new ApiError('Token isnt provided','Token','Oups Token is not provided')
+    }
     try {
         let user = await jwt.verify(token, process.env.TOKEN_SECRET);
         return user;    
     } catch (error) {
-        throw new Error('Error validating the token')
+       throw new ApiError('Token is not generated','Token',error.message)
     }
     
 }
