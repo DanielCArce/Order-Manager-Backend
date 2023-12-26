7455b6c42eacc7fe2e2204feca695cd9682b8845
DanielCArce <daniel.camposarce@gmail.com>
2023-12-25 21:22:05 -0600

    add: entry point
    adding all the most basic from the api

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import api from './src/server.js'
-
-const routes = ['api/v1/orders','api/v1/users','api/v1/companies','api/v1/auth', 'api/v1/']
-const server = api.listen(process.env.API_PORT,() => {
-    console.log(`Server is running in http://localhost:${server.address().port}${process.env.API_ROOT}/${process.env.API_LTS}/`)
-    console.log('Availables routes on the api: \n')
-    routes.forEach((route) => {
-        console.log(`http://localhost:${process.env.API_PORT}/${route}`)
-    })
-})
+import dotenv from 'dotenv'
+import app from './src/api.js'
+dotenv.config()
+const PORT = process.env.PORT || 3001
+app.listen(PORT, ()=> console.log(`http://localhost:${PORT}/api/v1/`))
