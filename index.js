9f3d1f19c3e35ca320cdafaa3cf6fa08d6897a04
DanielCArce <daniel.camposarce@gmail.com>
2023-12-28 16:05:05 -0600

    fix: change import name
    changing import declaration with update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
-import app from './src/api.js'
+import app from './src/server.js'
 dotenv.config()
 const PORT = process.env.PORT || 3001
 app.listen(PORT, ()=> console.log(`http://localhost:${PORT}/api/v1/`))
