f1f5ddf74a48464a11c69df496d0ff4636f6ca71
DanielCArce <daniel.camposarce@gmail.com>
2024-03-03 22:57:42 -0600

    add: auth middleware
    Add the middleware for protect all the routes
    that is not a public endpoint

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,4 +1,15 @@
-export default function AuthMiddleware(request, response,next) {
-    console.log({body: request.body})
-    next()
+import {isValidToken} from '../utils/jwt.js'
+export default async function AuthMiddleware(request, response, next) {
+    try {
+        const token = request.headers['authorization'].split(' ')[1]
+        const validationToken = await isValidToken(token)
+        console.log({token, validationToken})
+        if (validationToken) {
+            return next()
+        }
+        response.sendStatus(401)
+        console.log({ body: request.body })
+    } catch (error) {
+        next(error)
+    }
 }
