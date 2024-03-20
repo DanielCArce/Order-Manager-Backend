810d9181c210ce52a2d8c94c11b091295d4245e3
DanielCArce <daniel.camposarce@gmail.com>
2024-03-20 00:26:17 -0600

    updated ipand routes

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 import dotenv from 'dotenv'
+// import cluster from 'node:cluster'
+// import os from 'node:os'
+// import process from 'node:process'
+// const numb = os.availableParallelism()
 dotenv.config()
 import app from './src/server.js'
 const PORT = process.env.PORT || 3000
-const server = app.listen(PORT, () => {
-    console.log(`Server is running in http://localhost:${server.address().port}/`)
-})
+const HOST = process.env.HOST || '127.0.0.1'
+
+// if (cluster.isPrimary) { 
+//     console.log(`Master in ${process.pid}`)
+//     for (let i = 0; i < numb; i++){
+//         cluster.fork()
+//     }
+// } else {
+
+    const server = app.listen(PORT,HOST, () => {
+        console.log(`Server is running in http://${server.address().address}:${server.address().port}/`)
+    })
+//}
