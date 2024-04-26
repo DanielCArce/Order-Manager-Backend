5931ced39266a7800b9e5485b3cc776b27ff665a
DanielCArce <daniel.camposarce@gmail.com>
2024-04-26 13:50:47 -0600

    add new route in auth
    Add the route for get email password recovery
    tool

--- src/v1/routes/auth.js
+++ src/v1/routes/auth.js
@@ -1,9 +1,10 @@
 import { Router } from 'express'
 import AuthMiddleware from '../../middlewares/AuthMiddleware.js'
-import { authorizationCtrl } from '../controllers/auth.js'
+import { authorizationCtrl, recoveryPassord } from '../controllers/auth.js'
 
 const router = Router()
 
-router.post('/authorization',authorizationCtrl)
+router.post('/authorization', authorizationCtrl)
+    .post('/recovery-password', recoveryPassord)
 
 export default router
