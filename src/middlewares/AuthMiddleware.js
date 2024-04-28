821f1dc1c89719945866666a0b31141e78cfc58f
DanielCArce <daniel.camposarce@gmail.com>
2024-04-28 13:08:08 -0600

    add util function in middleware
    Remove old code instead use the function
    that preview extract to an util function

--- src/middlewares/AuthMiddleware.js
+++ src/middlewares/AuthMiddleware.js
@@ -1,11 +1,11 @@
-import {isValidToken} from '../utils/jwt.js'
+import { isValidToken } from '../utils/jwt.js'
+import {extractAuthorizationHeader} from '../utils/extractHeader.js'
 export default async function AuthMiddleware(request, response, next) {
     try {
-        const rawToken = request.headers['authorization']
-        if (rawToken == undefined || rawToken == null || rawToken == "") {
-            return response.sendStatus(401)
+        const token = await extractAuthorizationHeader(request.headers)
+        if (token == null) {
+            response.sendStatus(401)
         }
-        const token = rawToken.split(' ')[1]
         const isValid = await isValidToken(token)
         if (isValid) {
             return next()
