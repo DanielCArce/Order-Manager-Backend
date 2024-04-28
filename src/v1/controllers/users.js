59ed854334ff12b04a7f8f0245518ac20a7754c9
DanielCArce <daniel.camposarce@gmail.com>
2024-04-28 13:14:57 -0600

    fix method of update password
    Add the way of extract email from the token
    and  just need pass the new password
    from the app

--- src/v1/controllers/users.js
+++ src/v1/controllers/users.js
@@ -1,5 +1,7 @@
 import { createNewUser, updateUserInfo } from '../db/Users.js'
-import {hashPassword} from '../../utils/bcrypt.js'
+import { hashPassword } from '../../utils/bcrypt.js'
+import { extractAuthorizationHeader} from '../../utils/extractHeader.js'
+import {decodedToken} from '../../utils/jwt.js'
 export async function createUser(request, response, next) {
     try {
         const { name, email, password: rawPassword } = request.body
@@ -15,8 +17,9 @@ export async function createUser(request, response, next) {
 }
 export async function updatePassword(request,response, next) {
     try {
-        const { email: userEmailAPI, newPassword } = request.body
-        const user = await updateUserInfo(userEmailAPI, { password: await hashPassword(newPassword) })
+        const {newPassword } = request.body
+        const {payload} = await decodedToken(await extractAuthorizationHeader(request.headers))
+        const user = await updateUserInfo(payload.email, { password: await hashPassword(newPassword) })
         return response.status(200).json({user})
     } catch (error) {
         return next(error)
