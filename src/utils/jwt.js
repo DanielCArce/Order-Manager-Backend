82a262b8735784c8c7a0e9e569d3992a6432b938
DanielCArce <daniel.camposarce@gmail.com>
2024-04-28 13:02:41 -0600

    add method for decoded token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -6,3 +6,6 @@ export async function generateToken(payload) {
 export async function isValidToken(hashedToken) {
     return await JWT.verify(hashedToken, SECRET)
 }
+export async function decodedToken(token) {
+    return await JWT.decode(token)
+}
